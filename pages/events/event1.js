bdb6804806663b7c6962b173a131520fcfbbb3a3
Nuno Alves <nunowdev@gmail.com>
2023-07-01 08:35:24 +0100

    First Commit

---
+++ pages/events/event1.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Event1 = () => {
+  return <div>Event 1 Page</div>;
+};
+
+export default Event1;
---
+++ pages/events/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Event = () => {
+  return (
+    <div>
+      <a href="">
+        <img />
+        <h2>Events in London</h2>
+      </a>
+      <a href="">
+        <img />
+        <h2>Events in San Francisco</h2>
+      </a>
+      <a href="">
+        <img />
+        <h2>Events in Barcelona</h2>
+      </a>
+    </div>
+  );
+};
+
+export default Event;
